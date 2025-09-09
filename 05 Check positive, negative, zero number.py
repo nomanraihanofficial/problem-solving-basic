c8033d66730e541895f99f128311a848f0ac5e2c
Noman Raihan <mail.nomanraihan@gmail.com>
2025-09-09 15:12:48 +0600

    05 Check positive, negative, zero number

---
+++ 05 Check positive, negative, zero number.py
@@ -0,0 +1,15 @@
+"""
+    In your bank account,
+    +50 - Positive
+    -30 - Negative
+    0 - Zero balance
+"""
+
+num = - 10
+
+if num > 0:
+    print(f"${num} is positive number")
+elif num < 0:
+    print(f"${num} is negative number")
+else:
+    print(f"${num} is zero")
