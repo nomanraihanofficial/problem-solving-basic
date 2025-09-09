47675377586645dd08051cd8b932f8c1341ef508
Noman Raihan <mail.nomanraihan@gmail.com>
2025-09-09 15:20:23 +0600

    06 Print all numbers from 1 to N

---
+++ 06 Print all numbers from 1 to N.py
@@ -0,0 +1,7 @@
+"""
+    You have been given a number. You have to print from 1 to that particular number.
+"""
+
+N = 10          # The value of N can be anythings
+for i in range(1, N+1):
+    print(i)
