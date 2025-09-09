0853b2dfc407b8cecaa53215959b29421bdc536c
Noman Raihan <mail.nomanraihan@gmail.com>
2025-09-09 15:03:46 +0600

    04 Check even or odd number

---
+++ 04 Check even or odd number.py
@@ -0,0 +1,12 @@
+"""
+    If you have a number, you need to check whether this number is even or odd.
+    Remark: If a number is divisible by 2, it is an even number; otherwise, it is an odd number.
+"""
+
+a = 7
+
+
+if a % 2 == 0:
+    print("This is an even number")
+else:
+    print("This is an odd number")
