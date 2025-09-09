52f7fc128070863d82e6887fac8f49fcd755e4c8
Noman Raihan <mail.nomanraihan@gmail.com>
2025-09-09 11:32:31 +0600

    Find Maximum or Minimum of two numbers

---
+++ 02 Find maximum or minimum of two numbers.py
@@ -0,0 +1,16 @@
+"""
+    Imagine you have 2 values. You just want to compare which one is maximum or minimum number.
+    For maximum value, use the > sign.
+    For minimum value, use the < sign.
+
+"""
+
+a = 10
+b = 5
+
+if a > b:
+    print("a is maximum value")
+
+
+if a < b:
+    print("a is minimum value")
